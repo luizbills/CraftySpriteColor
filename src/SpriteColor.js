dea5e7ab58a8e8e407a21931718aea6c97ec6562
Luiz Paulo "Bills" Brandão <luizpbills@gmail.com>
2013-08-13 11:13:01 -0300

    Update SpriteColor.js

--- src/SpriteColor.js
+++ src/SpriteColor.js
@@ -5,55 +5,18 @@
 *
 * *Note: Only works for Canvas*
 */
-Crafty.c("SpriteColor", {
-	_spriteColorCanvas: null,
-	_color: "rgba(0,0,0,0)",
 
-	init: function(){
-		if (!this._spriteColorCanvas){
-			var c = document.createElement('canvas');
-			c.id = 'SpriteColorCanvas';
-			c.style.display = 'none';
-			Crafty.stage.elem.appendChild(c);
-			this._spriteColorCanvas = c;
-		}
-
-		this.bind("Draw", this._drawSpriteColor)
-			.bind("RemoveComponent", function(c) {
-				if (c === "SpriteColor") this.unbind("Draw", this._drawSpriteColor);
-			});
-	},
-
-	/**@
-	* #.spriteColor
-	* @comp SpriteColor
-	* @trigger Change - when the color is applied
-	* @sign public this .spriteColor(String hexcolor, Number strength)
-	* @param color - The color in HEXADECIMAL
-	* @param strength - Level of opacity
-	*
-	* The argument must be a color readable depending on which browser you
-	* choose to support. IE 8 and below doesn't support the rgb() syntax.
-	* 
-	* @example
-	* ~~~
-	* Crafty.sprite(16, "http://craftyjs.com/demos/tutorial/sprite.png", {player:[0,3]});
-	*
-	* Crafty.e("2D, Canvas, player, SpriteColor")
-	* 	.spriteColor("#FF0000",0.5); // red with 50% transparency
-	* ~~~
-	*/
-	spriteColor: function(hexcolor, strength){
-		this._color = Crafty.toRGB(hexcolor, strength);
-		this.trigger("Change");
-		return this;
-	},
-
-	_drawSpriteColor: function(){
+Crafty.spriteColorCanvas = (function() {
+	var sc_canvas = document.createElement('canvas'), // create an hidden canvas
+		sc_ctx = canvas.getContext('2d'),
+		sc_drawFunc;
+	
+	// draw callback
+	sc_drawFunc = function(){
 		// sprite coordinates
 		var co = this.__coord,
 		// context 2d of hidden canvas
-		ctx = this._spriteColorCanvas.getContext('2d');
+		ctx = sc_ctx;
 		// draw the sprite on hidden canvas
 		ctx.drawImage(this.img, //image element
 			co[0], //x position on sprite
@@ -73,6 +36,43 @@ Crafty.c("SpriteColor", {
 		ctx.closePath();
 		ctx.restore();
 		// draw the hidden canvas on Crafty canvas
-		Crafty.canvas.context.drawImage(this._spriteColorCanvas, this._x, this._y);
-	}
-});
+		Crafty.canvas.context.drawImage(sc_canvas, this._x, this._y);
+	};
+
+	// the component
+	Crafty.c("SpriteColor", {
+		_color: 'rgba(255,255,255,1)',
+
+		init: function(){
+			this.bind("Draw", sc_drawFunc)
+				.bind("RemoveComponent", function(c) {
+					if (c === "SpriteColor") this.unbind("Draw", sc_drawFunc);
+				});
+		},
+	
+		/**@
+		* #.spriteColor
+		* @comp SpriteColor
+		* @trigger Change - when the color is applied
+		* @sign public this .spriteColor(String hexcolor, Number strength)
+		* @param color - The color in HEXADECIMAL
+		* @param strength - Level of opacity
+		*
+		* The argument must be a color readable depending on which browser you
+		* choose to support. IE 8 and below doesn't support the rgb() syntax.
+		* 
+		* @example
+		* ~~~
+		* Crafty.sprite(16, "http://craftyjs.com/demos/tutorial/sprite.png", {player:[0,3]});
+		*
+		* Crafty.e("2D, Canvas, player, SpriteColor")
+		* 	.spriteColor("#FF0000",0.5); // red with 50% transparency
+		* ~~~
+		*/
+		spriteColor: function(hexcolor, strength){
+			this._color = Crafty.toRGB(hexcolor, strength);
+			this.trigger("Change");
+			return this;
+		}
+	});
+})();
